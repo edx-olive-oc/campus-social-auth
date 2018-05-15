7c878b7880a749b69b5c840a17877240fd49d34a
Diego Millan <diegor.millan@gmail.com>
2018-05-14 23:15:29 -0500

    Adding a config variable to get idp slug

--- campus_social_auth/backends/saml.py
+++ campus_social_auth/backends/saml.py
@@ -3,13 +3,14 @@ Slightly customized python-social-auth backend for SAML 2.0 support
 """
 
 from third_party_auth.saml import SAMLAuthBackend
+from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 
 from campus_social_auth.backends.utils import update_username_suggestion
 
 
 class CampusSAMLAuthBackend(SAMLAuthBackend):
     """
-    Customized version of SAMLIdentityProvider that can retrieve details beyond the standard
+    Customized version of SAMLAuthBackend that can retrieve details beyond the standard
     details supported by the canonical upstream version.
     """
 
@@ -18,8 +19,9 @@ class CampusSAMLAuthBackend(SAMLAuthBackend):
         Overrides `get_user_details` from the base class; retrieves those details,
         then updates the dict with values from whatever additional fields are desired.
         """
+        idp_slug = configuration_helpers.get_value('CAMPUS_SAML_PROVIDER_SLUG', '')
         details = super(CampusSAMLAuthBackend, self).get_user_details(attributes)
-        provider = self.get_idp(self.name)
+        provider = self.get_idp(idp_slug)
         details = update_username_suggestion(details, provider.conf)
 
         return details
